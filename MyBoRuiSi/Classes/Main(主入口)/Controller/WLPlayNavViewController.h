1708a3b716896130714eca16e716f5df140acec3
杨亮 <1572534835@qq.com>
2017-04-13 14:01:35 +0800

    fzg

---
+++ MyBoRuiSi/Classes/Main(主入口)/Controller/WLPlayNavViewController.h
@@ -0,0 +1,13 @@
+//
+//  WLPlayNavViewController.h
+//  MyBoRuiSi
+//
+//  Created by Magician on 2017/4/11.
+//  Copyright © 2017年 itcast.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WLPlayNavViewController : UINavigationController
+
+@end
---
+++ MyBoRuiSi/Classes/Main(主入口)/Controller/WLPlayNavViewController.m
@@ -0,0 +1,37 @@
+//
+//  WLPlayNavViewController.m
+//  MyBoRuiSi
+//
+//  Created by Magician on 2017/4/11.
+//  Copyright © 2017年 itcast.com. All rights reserved.
+//
+
+#import "WLPlayNavViewController.h"
+
+@interface WLPlayNavViewController ()
+
+@end
+
+@implementation WLPlayNavViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
